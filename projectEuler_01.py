852bd9b6ba6228f5dab6ee18103c6f426349ebe5
Melkemann84 <61843242+Melkemann84@users.noreply.github.com>
2020-11-17 11:06:09 +0100

    finished no.2

--- projectEuler_01.py
+++ projectEuler_01.py
@@ -1,13 +1,25 @@
-result = 0
+import time
+start_time = time.time()
+
+# https://projecteuler.net/problem=1
+# Multiples of 3 and 5
 
 def multiple(num):
     if num%3==0 or num%5==0:
         return num
     return 0
 
-    
+def Main():
+    result = 0
+    for n in range(1,1000):
+        result = result + multiple(n)
+    return result
 
-for n in range(1,1000):
-    result = result + multiple(n)
 
-print(result)
+answer = Main()
+
+
+print("--- %s seconds ---" % (time.time() - start_time))
+print("----------------------------------")
+print("result: " + str(answer))
+print("----------------------------------")
---
+++ projectEuler_template.py
@@ -0,0 +1,18 @@
+import time
+
+# https://projecteuler.net/problem=45
+# Triangular, pentagonal, and hexagonal
+
+def Main():
+
+    return ("🙁")
+
+
+
+start_time = time.time()
+answer = main()
+
+print("--- %s seconds ---" % (time.time() - start_time))
+print("----------------------------------")
+print("result: " + str(answer))
+print("----------------------------------")
