5710a5cb61fcc7f269d672f773c930de286dc14e
Melkemann84 <61843242+Melkemann84@users.noreply.github.com>
2023-07-28 21:10:44 +0200

    completed project 08

--- projectEuler_07.py
+++ projectEuler_07.py
@@ -8,7 +8,7 @@ we can see that the 6th prime is 13.
 
 What is the 10001st prime???
 '''
-def isPrime(num): #returns True if prime, False if not
+def isPrime(num): # Returns True if prime, False if not
     if num > 1:
         for i in range(2, int(num/2)+1): # Iterate from 2 to n / 2
             if (num % i) == 0:
@@ -31,7 +31,7 @@ def main(num):
     return max(factors)
 
 
-def showMe(answer,start_time): #returns answer in clipboard and calculates time used
+def showMe(answer,start_time): # Returns answer in clipboard and calculates time used
     print("----------------------------------")
     print("The answer: " + str(answer)+" has been copied to your clipboard!")
     print("--- %s seconds ---" % (time.time() - start_time))
