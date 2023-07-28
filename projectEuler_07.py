43623ec6e967d35190432a35bbdf5b7d81f52822
Melkemann84 <61843242+Melkemann84@users.noreply.github.com>
2023-07-28 19:41:15 +0200

    changed comments on line 9

--- projectEuler_07.py
+++ projectEuler_07.py
@@ -6,7 +6,7 @@ import pyperclip
 By listing the first six prime numbers: 2,3,5,7,11 and 13, 
 we can see that the 6th prime is 13.
 
-What is the 10001st prime?
+What is the 10001st prime??
 '''
 def isPrime(num): #returns True if prime, False if not
     if num > 1:
