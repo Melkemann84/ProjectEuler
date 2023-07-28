bd91a783307f901372195c4079eb97c85c4a2ce1
Melkemann84 <61843242+Melkemann84@users.noreply.github.com>
2023-07-28 18:33:08 +0200

    completed projectEuler_06

--- projectEuler_06.py
+++ projectEuler_06.py
@@ -1,17 +1,44 @@
 import time
+import pyperclip
 
-# https://projecteuler.net/problem=
+
+# https://projecteuler.net/problem=6
 '''
+The sum of the squares of the first ten natural numbers is,
+
+The square of the sum of the first ten natural numbers is,
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 
+.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 '''
+
+def sumOfSquares(n): #returns the sum of the squares of the range 0 to n
+    result = 0
+    for i in range (1,n+1):
+        result = result + i*i
+    return result
+
+def squareOfSum(n): #returns the square of the sum of the range 0 to n.
+    result = 0
+    for i in range (1,n+1): #sums the range
+        result = result + i   
+    result = result*result
+    return result
 
 def main():
-    result = 0
+    result = squareOfSum(100) - sumOfSquares(100) 
     return result
+
+def present(answer):
+    print("----------------------------------")
+    print("The answer: " + str(answer)+" has been copied to your clipboard!")
+    print("Time used: %s seconds ---" % (time.time() - start_time))
+    print("----------------------------------")
+    pyperclip.copy(str(answer))
+    spam = pyperclip.paste()
     
 start_time = time.time()
 answer = main()
-
-print("--- %s seconds ---" % (time.time() - start_time))
-print("----------------------------------")
-print("Answer: " + str(answer))
-print("----------------------------------")
+present(answer)
