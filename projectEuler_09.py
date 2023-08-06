56f34d90631b0fb7e587ad40988c1ac2c0ce128c
Melkemann84 <61843242+Melkemann84@users.noreply.github.com>
2023-08-06 02:02:48 +0200

    solved projevt 09

--- projectEuler_09.py
+++ projectEuler_09.py
@@ -9,31 +9,25 @@ There exists exactly one Pythagorean triplet for which a+b+c=1000. Find the prod
 '''
 
 # Function to check if A,B and C are Pythagorean numbers. A<C | B<C
-def isPythagorean(a,b,c):
-    if (a*a + b*b) == c*c:
+def isPythagorean(list):
+    if (list[0]*list[0] + list[1]*list[1]) == list[2]*list[2]:
         return True
     else:
         return False
-    
-# Function to check if list contains unique numbers.
-def isUnique(list):
-    list.sort()
-    list_is_unique = True
-    for i in range(len(list)-1):
-        if list[i] >= list[i+1]:
-            list_is_uniqie = False
-        if list_is_unique == False:
-            return False
-    return True
-    
 
-def main(sum_of_triplets):
-    a = b = c = 0
-    for c in range(sum_of_triplets/3+1):
-        c = c+1
-        for b in range(sum_of_triplets/3+1
-    result = 0
-    return result
+def main(target_sum_of_triplets):
+    triplets = [0,0,0]
+    for a in range(1,333): # A cannot be greater than 332<333<334<=1000
+        triplets[0] = a
+        for b in range(1,target_sum_of_triplets):
+            triplets[1] = b
+            for c in range(335,998): # C cannot be smaller than 335.
+                triplets[2] = c
+                if sum(triplets) == target_sum_of_triplets:
+                    if isPythagorean(triplets):
+                        print(triplets)
+                        return triplets[0]*triplets[1]*triplets[2]
+    return 0
 
 def showMe(answer,start_time): #returns answer in clipboard and calculates time used
     print("----------------------------------")
@@ -44,5 +38,5 @@ def showMe(answer,start_time): #returns answer in clipboard and calculates time 
     spam = pyperclip.paste()
     
 start_time = time.time()
-answer = main()
+answer = main(1000)
 showMe(answer,start_time)
