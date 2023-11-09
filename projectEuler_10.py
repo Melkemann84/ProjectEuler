f3d63a76ef7298d8f875b4f003cf96daab528f14
Melkemann84 <61843242+Melkemann84@users.noreply.github.com>
2023-11-10 00:04:44 +0100

    Finished pE_10. Optimised from 40min to 0.3sec

--- projectEuler_10.py
+++ projectEuler_10.py
@@ -6,26 +6,39 @@ import pyperclip
 The sum of the primes below 10 is 17.
 Find the sum of all the primes below two million.
 '''
+
+'''
+# using this version (not ready to be implemented as is, tho), code runs on BlackBear in aprox 40minutes
 def isPrime(num): # Returns True if prime, False if not
     if num > 1:
-        for i in range(2, int(num/2)+1): # Iterate from 2 to n / 2
-            if (num % i) == 0:
-                return False
-                break
+        if num == 2:
+            return True
+        if num != 2:
+            for i in range(3, int(num/2)+1, 2): # Iterate from 2 to n / 2
+                if (num % i) == 0:
+                    return False
+                    break
         else:
             return True
     else:
         return False
+'''
 
-def main(upper_limit):
-    list_of_primes = [0]                        #
-    for i in range(upper_limit):
-        if i % 1000 == 0:
-            print(f"We are currently at {i}")
-        if isPrime(i):
-            list_of_primes.append(i)
-    result = sum(list_of_primes)
-    return result
+def givePrimes(up_to):                          # Function returs array of all primes up to given limit
+    primes = [True for i in range(up_to+1)]     # Boolean array of True
+    p = 2                                       # 1 is not a prime
+    while (p * p <= up_to):                     # every in between p and p*p is covered by another prime already. 3 -> 9, because 4(2), 5(prime), 6(2,3), 7(p), 8(2)
+        if primes[p]:                           # if p of Primes == true, 
+            for i in range(p * p, up_to+1,p):   # update next multiples of p to False. We start at next square, because every in between p and p*p is covered by another prime already
+                primes[i] = False
+        p += 1
+    
+    list_of_primes = []
+    for p in range(2, up_to+1):                 #populate list where boolean array still is True
+        if primes[p]:
+            list_of_primes.append(p)
+    return list_of_primes
+
 
 
 def showMe(answer, start_time):  # returns answer in clipboard and calculates time used
@@ -38,5 +51,7 @@ def showMe(answer, start_time):  # returns answer in clipboard and calculates ti
 
 
 start_time = time.time()
-answer = main(2000000)
+temp = givePrimes(2000000)
+answer = sum(temp)
+print(temp)
 showMe(answer, start_time)
